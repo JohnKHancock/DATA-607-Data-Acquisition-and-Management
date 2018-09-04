4b4a2e7a7017ec166446fc0d2e831d29c47307af
JohnKHancock <jkhancock@gmail.com>
2018-09-03 20:09:05 -0400

    update

--- 02_Week Two Assignment/movies_db.sql
+++ 02_Week Two Assignment/movies_db.sql
@@ -5,8 +5,10 @@ CREATE TABLE Movies (
     ID INT NOT NULL,
     Title VARCHAR(255),
     Genre VARCHAR(255),
+    Male_Lead VARCHAR(255),
+    Female_Lead VARCHAR(255),
     Length TIME(3),
-    URL TEXT(65535),
+    Trailer TEXT (65535),
     PRIMARY KEY (ID)
 );
 
@@ -23,3 +25,20 @@ CREATE TABLE Reviewers (
         ON DELETE CASCADE
     
 );
+
+ALTER table movie_reviews.Reviewers
+ADD constraint rev_const
+CHECK(RATING > 1 AND RATING < 5);
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/reviewers.csv' 
+INTO TABLE reviews
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(country, year, sex, @child, @adult, @elderly)
+SET
+child = nullif(@child,-1),
+adult = nullif(@adult,-1),
+elderly = nullif(@elderly,-1)
+;
